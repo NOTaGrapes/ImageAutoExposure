4059ca53bc9003e27b2dc1377a9637932587da83
NOTaGrapes <vladislav-vinograd@yandex.com>
2022-12-27 23:20:39 +0300

    first commitq

---
+++ IAECore.h
@@ -0,0 +1,17 @@
+#ifndef IAECore
+#define IAECore
+
+#include <Windows.h>
+
+class  IAECore
+{
+public:
+	 IAECore();
+	 ~IAECore();
+
+private:
+
+};
+IAECore::IAECore();
+IAECore::~IAECore();
+#endif // !
